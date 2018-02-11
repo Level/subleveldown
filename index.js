f177c10140c7e10f3301f8e3b0a473c6114fb0a0
Finn Pauls <derfinn@gmail.com>
2018-02-11 23:27:24 +0100

    Update to abstract-leveldown 4 and levelup 2

--- index.js
+++ index.js
@@ -5,9 +5,5 @@ module.exports = function (db, prefix, opts) {
   if (typeof prefix === 'object' && !opts) return module.exports(db, null, prefix)
   if (!opts) opts = {}
 
-  opts.db = function () {
-    return subdown(db, prefix, opts)
-  }
-
-  return levelup(opts)
+  return levelup(subdown(db, prefix, opts), opts)
 }
