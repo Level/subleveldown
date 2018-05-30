259bac4e7bb0b79dd566752095362963931f8e7c
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-05-30 14:11:01 +0200

    Test SubDown constructor

--- test/index.js
+++ test/index.js
@@ -17,6 +17,50 @@ require('abstract-leveldown/abstract/close-test').close(down, test, testCommon)
 require('abstract-leveldown/abstract/iterator-test').all(down, test, testCommon)
 require('abstract-leveldown/abstract/ranges-test').all(down, test, testCommon)
 
+test('SubDown constructor', function (t) {
+  t.test('can be called without new', function (t) {
+    var sub = subdown()
+    t.is(sub instanceof subdown, true, 'instanceof subdown')
+    t.end()
+  })
+  t.test('missing prefix and missing separator', function (t) {
+    var sub = subdown()
+    t.is(sub.prefix, '!!')
+    t.end()
+  })
+  t.test('prefix and missing separator', function (t) {
+    var sub = subdown({}, 'prefix')
+    t.is(sub.prefix, '!prefix!')
+    t.end()
+  })
+  t.test('prefix and separator (as string)', function (t) {
+    var sub = subdown({}, 'prefix', '%')
+    t.is(sub.prefix, '%prefix%')
+    t.end()
+  })
+  t.test('prefix and separator (as options)', function (t) {
+    var sub = subdown({}, 'prefix', { separator: '%' })
+    t.is(sub.prefix, '%prefix%')
+    t.end()
+  })
+  t.test('prefix with same initial character as separator is sliced', function (t) {
+    var sub = subdown({}, '!prefix')
+    t.is(sub.prefix, '!prefix!')
+    t.end()
+  })
+  t.test('prefix with same ending character as separator is sliced', function (t) {
+    var sub = subdown({}, 'prefix!')
+    t.is(sub.prefix, '!prefix!')
+    t.end()
+  })
+  // TODO we're currently not guarded by multiple separators in the prefix
+  // t.test('repeated separator is slices off from prefix parameter', function (t) {
+  //   var sub = subdown({}, '!!prefix!!')
+  //   t.is(sub.prefix, '!prefix!')
+  //   t.end()
+  // })
+})
+
 function down (loc) {
   return subdown(levelup(loc, {db: memdown}), 'test')
 }
